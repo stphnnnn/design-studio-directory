a2015fcc48759c36a8b3509da6eb148ee992ef1e
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2019-12-07 21:58:40 +0000

    feat: add select field arrow

--- components/Select/Select.js
+++ components/Select/Select.js
@@ -17,12 +17,19 @@ const Input = styled.input`
   background-color: ${props => (!props.isCompact ? "#242F47" : "#1A2233")};
   color: white;
   font-weight: 600;
+
   &::placeholder {
     color: white;
     font-weight: 600;
     opacity: ${props => (props.isCompact ? 0.25 : undefined)};
   }
 `;
+
+const ArrowIcon = props => (
+  <svg height="20" width="20" viewBox="0 0 20 20" role="img" {...props}>
+    <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+  </svg>
+);
 
 const Select = ({
   disabled,
@@ -82,6 +89,19 @@ const Select = ({
               }}
               disabled={disabled}
             />
+            <ArrowIcon
+              style={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                right: "2rem",
+                fill: "white",
+                marginRight: "-2%",
+                opacity: isCompact ? 0.25 : undefined,
+                transform: `translateY(-50%) scaleY(${isOpen ? -1 : 1})`,
+                pointerEvents: "none"
+              }}
+            />
             <Input
               {...getInputProps({
                 disabled: true
@@ -123,53 +143,5 @@ const Select = ({
     </div>
   );
 };
-
-// import Select from 'react-select';
-
-// const options = [
-//   { value: 'chocolate', label: 'Chocolate' },
-//   { value: 'strawberry', label: 'Strawberry' },
-//   { value: 'vanilla', label: 'Vanilla' },
-// ];
-
-// const customStyles = {
-//   control: (styles, state) => ({
-//     ...styles,
-//     width: 350,
-//     height: state.isDisabled ? 75 : 100,
-//     padding: '1rem',
-//     borderRadius: 0,
-//     border: 'none',
-//     backgroundColor: state.isDisabled ? '#1A2233' : '#242F47',
-//   }),
-//   dropdownIndicator: (styles, state) => ({
-//     ...styles,
-//     opacity: state.isDisabled ? 0.25 : 1,
-//   }),
-//   placeholder: (styles, state) => ({
-//     ...styles,
-//     color: 'white',
-//     fontWeight: 600,
-//     opacity: state.isDisabled ? 0.25 : 1,
-//   }),
-//   indicatorSeparator: styles => ({ display: 'none' }),
-// };
-
-// const CustomSelect = ({ label, disabled }) => (
-//   <div
-//     style={{
-//       display: 'flex',
-//       alignItems: 'center',
-//     }}
-//   >
-//     <Select
-//       label={label}
-//       placeholder={label}
-//       styles={customStyles}
-//       options={options}
-//       isDisabled={disabled}
-//     />
-//   </div>
-// );
 
 export default Select;
