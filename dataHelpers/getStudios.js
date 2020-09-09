93196da54cedde55d498c981d7ae9039c26b84c6
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-09-09 15:13:10 +0100

    remove sheety

--- dataHelpers/getStudios.js
+++ dataHelpers/getStudios.js
@@ -2,40 +2,38 @@ import { parse } from "date-fns";
 import compareDesc from "date-fns/compareDesc";
 import shortid from "shortid";
 
-const createDate = dateString => {
+const createDate = (dateString) => {
   return parse(dateString || "1960-06-06", "yyyy-MM-dd", new Date());
 };
 
-const getStudios = data => {
+const getStudios = (data) => {
   return data
-    .map(studio => {
-      const name = studio.companyName && studio.companyName.toString();
+    .map((studio) => {
+      const name = studio["Company Name"] && studio["Company Name"].toString();
 
       const cityEntries = Object.entries(studio).filter(
-        ([cityKey, city]) => cityKey.indexOf("city") === 0 && city
+        ([cityKey, city]) => cityKey.indexOf("City") === 0 && city
       );
 
       const locations = cityEntries.map(([cityKey, city]) => {
         const locationId = parseInt(cityKey.substr(4));
-        const country = studio[`country${locationId}`];
+        const country = studio[`Country ${locationId}`];
 
         return {
           city,
-          country
+          country,
         };
       });
 
       const dateAdded = createDate(studio.dateAdded);
-
-      const { twitterHandle, url } = studio;
 
       return {
         id: shortid.generate(),
         name,
         locations,
         dateAdded,
-        twitterHandle,
-        url
+        twitterHandle: studio["Twitter Handle"],
+        url: studio.Url,
       };
     })
     .sort((a, b) => compareDesc(a.dateAdded, b.dateAdded));
